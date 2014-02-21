e681f86aaefe030e47741f24b7f78750af6bbea5
Òscar Vilaplana <dev@oscarvilaplana.cat>
2014-02-21 20:47:51 +0100

    app, south

--- explosion/settings.py
+++ explosion/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fun',
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
